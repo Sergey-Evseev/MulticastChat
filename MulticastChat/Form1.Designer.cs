b7d85b4a746a37d0d61f5a0cb0afd49901d3561b
Sergey Evseev <sevseev@gmail.com>
2023-04-20 00:35:22 +0300

    Add project files.

---
+++ MulticastChat/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MulticastChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timerHeartbeat = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(35, 27);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(355, 186);
+            this.txtChat.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(35, 258);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(355, 66);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(89, 361);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(240, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.Location = new System.Drawing.Point(431, 53);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(418, 199);
+            this.lstUsers.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CHAT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MESSAGE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(598, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ACTIVE USERS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 441);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstUsers);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timerHeartbeat;
+    }
+}
+
---
+++ MulticastChat/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MulticastChat
+{
+    public partial class Form1 : Form
+    {
+        //Multicast group address and port number
+        //The multicast address 239.0.0.1 is a reserved address used for "all hosts
+        //on this subnet". It is a special address that is used for local network
+        //communication, and it is not routable over the Internet.
+        private IPAddress multicastAddress = IPAddress.Parse("239.0.0.1");
+        private int multicastPort = 8000;
+
+        //UDP client and multicast endpoint:
+        //This variable will be used to send and receive data to/from the multicast group.
+        private UdpClient udpClient;
+        //This variable will be used to specify the destination for the multicast messages
+        //sent by the UdpClient.
+        private IPEndPoint multicastEndPoint;
+
+        //Thread for receiving messages
+        //new thread that runs in the background to receive messages sent to the multicast group
+        //program can continue to execute other tasks without being blocked while waiting
+        //for incoming messages
+        private Thread receiveThread;
+
+        //User name and heartbeat interval
+        //variable that will be used to store the name of the user or device that is sending the heartbeat messages.
+        private string userName;
+        //interval for sending heartbeat messages, in milliseconds
+        private int heartbeatInterval = 5000;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Create UDP client and multicast endpoint
+            udpClient = new UdpClient();
+            //join the client to the multicast group specified by the multicastAddress variable
+            //This tells the operating system that the UDP client is interested in receiving
+            //messages sent to the specified multicast group.
+            udpClient.JoinMulticastGroup(multicastAddress);
+            //This endpoint will be used to SEND and RECEIVE data to/from the multicast group
+            //using the UdpClient.
+            multicastEndPoint = new IPEndPoint(multicastAddress, multicastPort);
+
+            //Start thread for receiving messages
+            //starts a new thread to run the ReceiveMessages method in the background
+            //The constructor takes a delegate that represents the method that will be executed
+            //in the new thread.  In this case, the delegate is created using the ThreadStart
+            //constructor, which takes a reference to the ReceiveMessages method.
+            receiveThread = new Thread(new ThreadStart(ReceiveMessages));
+            receiveThread.Start();
+        }
+
+        private void ReceiveMessages()
+        {
+            //Create buffer for receiving messages
+            byte[] receiveBuffer = new byte[1024];
+
+            //Start loop for receiving messages
+            while (true)
+            {
+                //Receive message from multicast group
+                IPEndPoint remoteEndPoint = new IPEndPoint()
+            }
+}
+    }
+}
